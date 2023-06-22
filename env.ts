2335e6af993964c8002b3066894d4a2272595b53
Femi Ayo-Tubosun <thefemiolatubosun@gmail.com>
2023-06-22 22:26:44 +0100

    chore: update environment variables

--- env.ts
+++ env.ts
@@ -24,6 +24,11 @@ export default Env.rules({
   DRIVE_DISK: Env.schema.enum(['local'] as const),
   NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
 
+  OTP_TOKEN_EXPIRES_IN_X_MINUTES: Env.schema.number(),
+  OTP_TOKEN_LENGTH: Env.schema.number(),
+  MAXIMUM_CONSECUTIVE_FAILED_LOGIN_ATTEMPTS: Env.schema.number(),
+
+  DB_CONNECTION: Env.schema.string(),
   PG_HOST: Env.schema.string({ format: 'host' }),
   PG_PORT: Env.schema.number(),
   PG_USER: Env.schema.string(),

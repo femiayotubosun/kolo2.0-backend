490b56e8058539812bb456162e44bc2ca12ec6f8
Femi Ayo-Tubosun <thefemiolatubosun@gmail.com>
2023-06-22 22:27:19 +0100

    feat: add UserProfile and Registration Steps

--- app/Project/Client/UserManagement/Migrations/1687466016756_users.ts
+++ app/Project/Client/UserManagement/Migrations/1687466016756_users.ts
@@ -5,11 +5,20 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.increments('id').primary()
+      table.string('identifier').notNullable()
+      table.string('email').notNullable().unique()
+      table.string('first_name').notNullable()
+      table.string('last_name').notNullable()
+      table.string('password', 180).nullable()
+      table.string('mobile_number').notNullable()
+      table.string('remember_me_token').nullable()
+      table.dateTime('last_login_date')
+      table.boolean('is_first_time_login').defaultTo(1)
+      table.boolean('has_verified_email').defaultTo(0)
+      table.integer('login_attempts').defaultTo(0)
+      table.boolean('is_active').notNullable().defaultTo(0)
+      table.boolean('is_deleted').notNullable().defaultTo(0)
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
--- app/Project/Client/UserManagement/Models/User.ts
+++ app/Project/Client/UserManagement/Models/User.ts
@@ -1,7 +1,84 @@
-import { column } from '@ioc:Adonis/Lucid/Orm'
+import { beforeSave, column, computed, hasOne, HasOne } from '@ioc:Adonis/Lucid/Orm'
 import AbstractModel from 'App/Common/Models/AbstractBaseModel'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { DateTime } from 'luxon'
+import UserProfile from 'App/Project/Client/UserManagement/Models/UserProfile'
+import UserRegistrationStep from 'App/Project/Client/UserManagement/Models/UserRegistrationStep'
 
 export default class User extends AbstractModel {
-  @column({ isPrimary: true })
-  public id: number
+  @column()
+  public email: string
+
+  @column()
+  public firstName: string
+
+  @column()
+  public lastName: string
+
+  @computed()
+  public get fullName(): string {
+    return `${this.firstName} ${this.lastName}`
+  }
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public mobileNumber: string
+
+  @column()
+  public rememberMeToken: string | null
+
+  @column.dateTime({
+    serialize: (dateValue: DateTime | null) => {
+      return dateValue ? dateValue.setZone('utc').toLocaleString(DateTime.DATETIME_FULL) : dateValue
+    },
+  })
+  public lastLoginDate: DateTime
+
+  @column({
+    consume: (value: number) => (value === 1 ? 'Yes' : 'No'),
+  })
+  public isFirstTimeLogin: boolean | string
+
+  @column({
+    consume: (value: number) => (value === 1 ? 'Yes' : 'No'),
+  })
+  public hasVerifiedEmail: boolean | string
+
+  @column()
+  public loginAttempts: number
+
+  @column({
+    consume: (value: number) => (value === 1 ? 'Yes' : 'No'),
+  })
+  public isActive: boolean | string
+
+  @column({
+    consume: (value: number) => (value === 1 ? 'Yes' : 'No'),
+  })
+  public isDeleted: boolean | string
+
+  @hasOne(() => UserProfile)
+  public profile: HasOne<typeof UserProfile>
+
+  @hasOne(() => UserRegistrationStep)
+  public userRegistrationStep: HasOne<typeof UserRegistrationStep>
+  //
+  // @hasOne(() => Wallet, {
+  //   foreignKey: 'userId',
+  // })
+  // public akibaWallet: HasOne<typeof Wallet>
+  //
+  // @hasMany(() => StandardSaving, {
+  //   foreignKey: 'userId',
+  // })
+  // public standardSavings: HasMany<typeof StandardSaving>
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
 }

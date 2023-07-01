ba75f4aec2a5985548f9f9d4e658bc93ebaa141d
Femi Olatubosun | ƒa†3 <femi@zerocomple.ai>
2023-07-01 19:21:09 +0100

    feat: create fetch user profile endpoint

--- app/Project/Client/UserManagement/Routes/ProfileManagementRoutes.ts
+++ app/Project/Client/UserManagement/Routes/ProfileManagementRoutes.ts
@@ -1,7 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get('Process/Fetch/UserProfileDetails', 'FetchUserProfileDetailsController').as(
+  Route.get('Fetch/UserProfileDetails', 'FetchUserProfileDetailsController').as(
     'fetch-user-profile-details'
   )
 })
